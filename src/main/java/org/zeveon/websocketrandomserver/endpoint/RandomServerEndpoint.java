31ef0268a1be84ccb81b0f19685a22441ed2eea1
Vafin Stanislav <stasvafin@gmail.com>
2023-06-01 20:19:30 +0700

    refactoring

--- src/main/java/org/zeveon/websocketrandomserver/endpoint/RandomServerEndpoint.java
+++ src/main/java/org/zeveon/websocketrandomserver/endpoint/RandomServerEndpoint.java
@@ -3,6 +3,7 @@ package org.zeveon.websocketrandomserver.endpoint;
 import com.google.gson.Gson;
 import jakarta.websocket.*;
 import jakarta.websocket.server.ServerEndpoint;
+import lombok.extern.slf4j.Slf4j;
 import org.zeveon.websocketrandomserver.config.GetHttpSessionConfigurator;
 import org.zeveon.websocketrandomserver.model.RandomResponse;
 
@@ -13,61 +14,80 @@ import java.util.Random;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
+import static java.util.Optional.ofNullable;
 import static org.zeveon.websocketrandomserver.util.StringUtil.REMOTE_ADDRESS;
 
 /**
  * @author Stanislav Vafin
  */
+@Slf4j
 @ServerEndpoint(
         value = "/random",
         configurator = GetHttpSessionConfigurator.class
 )
 public class RandomServerEndpoint {
 
-    private static final Random random = new Random();
-    private static final Map<Session, Set<BigInteger>> sessionBigIntegerMap = new ConcurrentHashMap<>();
-    private static final Gson gson = new Gson();
+    private static final CloseReason ALREADY_CONNECTED_CLOSE_REASON =
+            new CloseReason(CloseReason.CloseCodes.VIOLATED_POLICY, "Already connected");
+    private static final Map<Session, Set<BigInteger>> SESSION_NUMBERS_MAP = new ConcurrentHashMap<>();
+    private static final Random RANDOM = new Random();
+    private static final Gson GSON = new Gson();
+    private static final int NUM_BITS = 130;
 
     @OnOpen
     public void onOpen(Session session) {
         var ipAddress = getIpAddress(session);
-
-        for (Session other : session.getOpenSessions()) {
-            if (other.isOpen() && ipAddress.equals(getIpAddress(other))) {
-                try {
-                    session.close(new CloseReason(CloseReason.CloseCodes.VIOLATED_POLICY, "Already connected"));
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                return;
-            }
+        if (isSameIp(session, ipAddress)) {
+            closeSession(session, ALREADY_CONNECTED_CLOSE_REASON);
+            return;
         }
-
-        sessionBigIntegerMap.put(session, new HashSet<>());
+        SESSION_NUMBERS_MAP.put(session, new HashSet<>());
     }
 
     @OnMessage
     public String onMessage(Session session, String message) {
-        var bigIntegers = sessionBigIntegerMap.get(session);
-        BigInteger value;
-        do {
-            value = new BigInteger(130, random);
-        } while (bigIntegers.contains(value));
-        bigIntegers.add(value);
-        return gson.toJson(new RandomResponse(value.toString()));
+        var existingNumbers = SESSION_NUMBERS_MAP.get(session);
+        var generatedNumber = generateRandomNumber(existingNumbers);
+        existingNumbers.add(generatedNumber);
+        return GSON.toJson(new RandomResponse(generatedNumber.toString()));
     }
 
     @OnClose
     public void onClose(Session session) {
-        sessionBigIntegerMap.remove(session);
+        SESSION_NUMBERS_MAP.remove(session);
     }
 
     @OnError
     public void onError(Session session, Throwable throwable) {
-        throwable.printStackTrace();
+        log.error("Something went wrong in session %s, session is closed".formatted(session.getId()), throwable);
     }
 
     private String getIpAddress(Session session) {
-        return session.getUserProperties().get(REMOTE_ADDRESS).toString();
+        return ofNullable(session.getUserProperties().get(REMOTE_ADDRESS))
+                .map(Object::toString)
+                .orElseThrow(() -> new RuntimeException("Remote address not found"));
+    }
+
+    private boolean isSameIp(Session session, String ipAddress) {
+        return session.getOpenSessions().stream()
+                .filter(Session::isOpen)
+                .anyMatch(other -> ipAddress.equals(getIpAddress(other)));
+    }
+
+    @SuppressWarnings("SameParameterValue")
+    private void closeSession(Session session, CloseReason closeReason) {
+        try {
+            session.close(closeReason);
+        } catch (Exception exception) {
+            log.error("Error during session close", exception);
+        }
+    }
+
+    private BigInteger generateRandomNumber(Set<BigInteger> bigIntegers) {
+        BigInteger value;
+        do {
+            value = new BigInteger(NUM_BITS, RANDOM);
+        } while (bigIntegers.contains(value));
+        return value;
     }
 }
